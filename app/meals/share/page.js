30617d98259af559a4c019e44b43e547dda207ce
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-03-06 10:20:18 +0700

    🏷️ Form Submitting and Storing Uploaded images & storing data in the database
    Form Submitting and Storing Uploaded images & storing data in the database

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,4 +1,7 @@
+import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
   return (
@@ -10,7 +13,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -38,9 +41,9 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
